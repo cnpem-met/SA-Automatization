2df692a3726a967fb18a5c15d95ee0a81f92e61c
Rodrigo de Oliveira Neto <rodrigo.neto@lnls.br>
2020-06-30 18:29:15 -0300

    inserindo método para cálculo de desvios angulares

--- src/Automatization-SA.py
+++ src/Automatization-SA.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def rotate(P, Rx, Ry, Rz):
     rot_z = np.array([[np.cos(Rz*10**-3), -np.sin(Rz*10**-3), 0], [np.sin(Rz*10**-3), np.cos(Rz*10**-3), 0], [0, 0, 1]])
@@ -233,6 +234,173 @@ def append_values (calc_operation, x_list, y_list, z_list, dataset, current_gird
         
     return x_list, y_list, z_list
 
+def calculate_angles (dataset):
+    
+    angles_df = pd.DataFrame(columns=['Girder', 'Roll', 'Pitch', 'Yaw'])
+    
+    i = 0
+    pts_girder = []
+    pts_name = []
+    
+    while (i<dataset[0].size):
+        
+        current_girder = dataset.loc[i,0][:7]
+        point_name = dataset.loc[i,0][8:]
+        
+        # adiciona na lista todos os pontos de um berço, exceto no caso de B03 e B11,
+        # em que só será adicionado os pontos do multipolo
+        if (current_girder[4:] == 'B03' or current_girder[4:] == 'B11'):
+            if (point_name[-2:] != "B1" and point_name[-2:] != "MR"):
+                pts_girder.append([dataset.loc[i,1], dataset.loc[i,2], dataset.loc[i,3]])
+                pts_name.append(point_name)
+        else:
+            pts_girder.append([dataset.loc[i,1], dataset.loc[i,2], dataset.loc[i,3]])
+            pts_name.append(point_name)
+        
+        
+        # começando a iteração sobre os outros pontos do mesmo berço
+        j = i+1
+        while (current_girder == dataset.loc[j,0][:7]):
+            point_name = dataset.loc[j,0][8:]
+            
+            # mesma regra se aplica
+            if (current_girder[4:] == 'B03' or current_girder[4:] == 'B11'):
+                if (point_name[-2:] != "B1" and point_name[-2:] != "MR"):
+                    pts_girder.append([dataset.loc[j,1], dataset.loc[j,2], dataset.loc[j,3]])
+                    pts_name.append(point_name)
+            else:
+               pts_girder.append([dataset.loc[j,1], dataset.loc[j,2], dataset.loc[j,3]])
+               pts_name.append(point_name)
+            
+            j+=1
+            
+            # se proximo ponto for o ultimo da lista
+            if (j>=dataset[0].size):
+                break
+            
+
+        """ tratamento de exceção de menos de 4 pontos medidos nesse berço """
+        # verifica se no caso do B1 e Multipolo há menos de 4 pontos
+        if ((current_girder[4:] != 'B05' and  current_girder[4:] != 'B09') and len(pts_girder)<4):
+            error_txt = "exceção encontrada no berço "+current_girder+": "
+            if (len(pts_girder) == 3):
+                error_txt += "3 pontos medidos p/ se calcular os erros angulares."
+                
+                if (current_girder[4:] != 'B11'):
+                    pts = ['C01', 'C02', 'C03', 'C04']
+                else:
+                    pts = ['C09', 'C10', 'C11', 'C12']
+                    
+                if(not pts[0] in pts_name):
+                    # coordenadas do C01/C09 construido: [C04(x), C02(y), C02(z)]
+                    # ** assume-se novamente que os pontos estão ordenados!
+                    pt_C01_C09 = [pts_girder[2][0], pts_girder[0][1], pts_girder[0][2]]
+                    pts_girder.append(pt_C01_C09)
+                    pts_name.append(pts[0])
+                elif (not pts[1] in pts_name):
+                    # coordenadas do C02 construido: [C03(x), C01(y), C01(z)]
+                    pt_C02_C10 = [pts_girder[1][0], pts_girder[0][1], pts_girder[0][2]]
+                    pts_girder.append(pt_C02_C10)
+                    pts_name.append(pts[1])
+                elif (not pts[2] in pts_name):
+                    # coordenadas do C03 construido: [C02(x), C04(y), C04(z)]
+                    pt_C03_C11 = [pts_girder[1][0], pts_girder[2][1], pts_girder[2][2]]
+                    pts_girder.append(pt_C03_C11)
+                    pts_name.append(pts[2])
+                else:
+                    # coordenadas do C04 construido: [C01(x), C03(y), C03(z)]
+                    pt_C03_C12 = [pts_girder[0][0], pts_girder[2][1], pts_girder[2][2]]
+                    pts_girder.append(pt_C03_C12)
+                    pts_name.append(pts[3])
+               
+                    
+            elif (len(pts_girder) == 2):
+                error_txt += "2 pontos medidos p/ se calcular os erros angulares."
+                """ tratamento de exceção para apenas 2 pontos? """
+                pass
+            
+            print(error_txt)
+                
+            
+        """ calculo dos angulos"""
+        if (current_girder[4:] != 'B05' and  current_girder[4:] != 'B09'):
+            # operação necessária pois a lista pts_girder pode estar desordenada
+            if (current_girder[4:] != 'B11'):
+                pts = ['C01', 'C02', 'C03', 'C04']
+            else:
+                pts = ['C09', 'C10', 'C11', 'C12']
+            
+            
+            index_C1 = pts_name.index(pts[0])
+            index_C2 = pts_name.index(pts[1])
+            index_C3 = pts_name.index(pts[2])
+            index_C4 = pts_name.index(pts[3])
+            
+            C1 = pts_girder[index_C1] # aqui C1 pode representar tanto C1 (caso dos multipolos) quanto C9 (caso dos B1)
+            C2 = pts_girder[index_C2] # o mesmo se aplica para C2, C3 e C4
+            C3 = pts_girder[index_C3]
+            C4 = pts_girder[index_C4]
+            
+            roll_tan = (np.mean([C2[2], C3[2]]) - np.mean([C1[2], C4[2]])) / (np.mean([C1[0], C4[0]]) - np.mean([C2[0], C3[0]])) 
+            roll = math.atan(roll_tan) *10**3 # em mrad
+            
+            pitch_tan = (np.mean([C3[2], C4[2]]) - np.mean([C1[2], C2[2]])) / (np.mean([C3[1], C4[1]]) - np.mean([C1[1], C2[1]]))
+            pitch = math.atan(pitch_tan) *10**3 # mrad
+            
+            yaw_tan = (np.mean([C3[0], C4[0]]) - np.mean([C1[0], C2[0]])) / (np.mean([C3[1], C4[1]]) - np.mean([C1[1], C2[1]]))
+            yaw = math.atan(yaw_tan) *10**3 # mrad
+        
+        else: # caso dos B2
+            # considera-se que os pontos estão ordenados
+            error = False
+            try:
+                B2, B2MR, LV01, LV02, LV03 = pts_girder
+            except ValueError:
+                print("exceção encontrada no berço "+current_girder+" to tipo B2: menos de 5 pontos no total.\n Calculo de angulos falhou para esse berço")
+                error = True
+                
+            if(error):
+                roll = pitch = yaw = -999 # valor para elucidar que houve erro no cálculo para esse berço
+            else:
+                roll_tan = (np.mean([LV01[2], LV03[2]]) - LV02[2]) / (LV02[0] - np.mean([LV01[0], LV03[0]]))
+                roll = math.atan(roll_tan)
+                
+                pitch_tan = (LV03[2] - LV01[2]) / (LV03[1] - LV01[1])
+                pitch = math.atan(pitch_tan)
+                
+                # transladando B2 para a origem do frame
+                B2_new = np.array([[B2[0]], [B2[1]], [B2[2]]])
+                B2_new = translate(B2_new, -B2[0], -B2[1], -B2[2])
+                B2_new = [B2_new[0,0], B2_new[1,0], B2_new[2,0]]
+                
+                # aplicando o mesmo shift em B2MR, e rotacionando em 20 mrad
+                B2MR_new = np.array([[B2MR[0]], [B2MR[1]], [B2MR[2]]])
+                B2MR_new = translate(B2MR_new, -B2[0], -B2[1], -B2[2])
+                B2MR_new = rotate(B2MR_new, 0, 0, 20)
+                B2MR_new = [B2MR_new[0,0], B2MR_new[1,0], B2MR_new[2,0]]
+                
+                yaw_tan = (B2MR_new[0] - B2_new[0]) / (B2MR_new[1] - B2_new[1])
+                yaw = math.atan(yaw_tan)
+                
+
+                
+        # salvando angulos em DataFrame
+        angle_data = pd.DataFrame(data=np.array([[current_girder, roll, pitch, yaw]]),columns=['Girder', 'Roll', 'Pitch', 'Yaw'])
+        angles_df = angles_df.append(angle_data, ignore_index=True)
+        
+        i = j
+        pts_girder = []
+        pts_name = []
+    
+    # torna os itens da coluna 'girder' nos índices do dataframe,
+    # necessário para usar as funções de cálculo aritimético entre dataframes
+    angles_df = angles_df.set_index('Girder')
+    
+    # retorna o df com os termos no tipo numérico certo
+    return angles_df.astype('float32')
+
+
+
 def calculate_centroids (dataset, pts_type):
 
     # DataFrame que conterá os centroids ao final dessa função
@@ -443,6 +611,11 @@ if __name__ == "__main__":
     
     inOut_diff = calc_df_diff(inOut_nominal, inOut_measured)
     
+    #%%
+    
+    rotational_devs = calculate_angles(local_measured)
+    
+    print(rotational_devs[:20])
 
 
 '''
